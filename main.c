68979f141a0dd23871da609b973aa50b047342d4
Spheroman <jackwen04@gmail.com>
2022-12-08 19:36:50 +0800

    week 11

--- main.c
+++ main.c
@@ -1,49 +1,77 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
+char *replace(char *source, char *pattern, char *replacement)
+{
+    int len = 0, replacelen = 0, patternlen = 0, e;
+    while(source[len] != 0)
+        len++;
+    while(pattern[patternlen] != 0)
+        patternlen++;
+    while(replacement[replacelen] != 0)
+        replacelen++;
+    int i, j;
+    for (i = 0; source[i] != '\0'; i++){
+        if (source[i] == pattern[0]){
+            for (j = 0; pattern[j] != '\0'; j++){
+                if (source[i + j] != pattern[j])
+                    break;
+            }
+            if (pattern[j] == '\0')
+                break;
+        }
+    }
+    if (source[i] == '\0')
+        return source;
+    char *ret = malloc(len + replacelen - patternlen + 1);
+    for(e = 0; e < i; e++){
+        ret[e] = source[e];
+    }
+    for(e = i; e - i < replacelen; e++){
+        ret[e] = replacement[e - i];
+    }
+    while(source[e - replacelen + patternlen] != '\0'){
+        ret[e] = source[e - replacelen + patternlen];
+        e++;
+    }
+    ret[e] = '\0';
+    return ret;
+}
+
+
+char *replaceAll(char *source, char *pattern, char *replacement)
+{
+    int len = 0, replacelen = 0, patternlen = 0, e;
+    while(source[len] != 0)
+        len++;
+    while(pattern[patternlen] != 0)
+        patternlen++;
+    while(replacement[replacelen] != 0)
+        replacelen++;
+    int i, j;
+    for (i = 0; source[i] != '\0'; i++){
+        if (source[i] == pattern[0]){
+            for (j = 0; pattern[j] != '\0'; j++){
+                if (source[i + j] != pattern[j])
+                    break;
+            }
+            if (pattern[j] == '\0')
+                break;
+        }
+    }
+    if (source[i] == '\0')
+        return source;
+    char *ret = malloc(len + replacelen - patternlen + 1);
+    
+}
 
 int main() {
-    int c, s, i, j, fail = 0, avg = 0, tot, temp, stud = 0;
-    scanf("%i %i", &c, &s);
-    int courses[c][3];
-    for(i = 0; i < c; i++){
-        for(j = 0; j < 3; j++){
-            scanf("%i", &courses[i][j]);
-        }
-    }
-    int scores[s][2];
-    for(i = 0; i < s; i++){
-        for(j = 0; j < 2; j++){
-            scanf("%i", &scores[i][j]);
-        }
-    }
-    int students[c];
-    for(i = 0; i < c; i++){
-        temp = 0;
-        for(j = 0; j < c; j++)
-            if(students[j] == courses[i][0])
-                temp = 1;
-        if(!temp){
-            students[i] = courses[i][0];
-            stud++;
-        }
-    }
-
-    for(i = 0; i < stud; i++){
-        tot = 0;
-        avg = 0;
-        for(j = 0; scores[j][0] != students[i]; j++);
-        while(j < s){
-            while(scores[j][0] == students[i]){
-                avg+=scores[j][1];
-                tot+=60;
-                j++;
-            }
-            j++;
-        }
-        if(avg < tot)
-            fail++;
-    }
-    printf("%i %i", stud, fail);
+    char source[100], pattern[100], replacement[100];
+    scanf("%s", source);
+    scanf("%s", pattern);
+    scanf("%s", replacement);
+    char *done = replaceAll(source, pattern, replacement);
+    printf("%s", done);
 
 }
